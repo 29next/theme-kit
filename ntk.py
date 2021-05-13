e281897ee45a949403327f6d19ff54e676672384
bell <bell@29next.com>
2021-05-13 17:06:12 +0700

    Support multiple environment

--- ntk.py
+++ ntk.py
@@ -5,6 +5,7 @@ import collections
 import logging
 import os
 import requests
+import time
 
 from watchgod import awatch
 from watchgod.watcher import Change
@@ -15,67 +16,84 @@ CONFIG_FILE = os.path.join(os.getcwd(), CONFIG_FILE_NAME)
 CONTENT_FILE_EXTENSIONS = ['.html', '.json', '.css', '.js']
 MEDIA_FILE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.woff', '.woff2', '.ico']
 
-ConfigInfo = collections.namedtuple('ConfigInfo', 'apikey theme_id store')
+Config = collections.namedtuple('Config', 'env apikey theme_id store')
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
 
 def _validate_config(apikey, theme_id, store):
+    error_msgs = []
     if not apikey:
-        raise TypeError('argument -a/--apikey is required')
+        error_msgs.append('-a/--apikey')
     if not theme_id:
-        raise TypeError('argument -t/--theme_id is required')
+        error_msgs.append('-t/--theme_id')
     if not store:
-        raise TypeError('argument -s/--store is required')
+        error_msgs.append('-s/--store')
+
+    if error_msgs:
+        message = ', '.join(error_msgs)
+        pluralize = 'is' if len(error_msgs) == 1 else 'are'
+        raise TypeError(f'argument {message} {pluralize} required')
 
 
-def _get_config(parser=None):
-    # default config structure
-    config = {
-        "development": {
-            "apikey": None,
-            "theme_id": None,
-            "store": None
-        }
-    }
-    is_config_update = False
-
+def _get_config(parser, write_file=False):
+    configs = {}
+    env = parser.env
     if not os.path.exists(CONFIG_FILE):
-        logging.info(f'Creating {CONFIG_FILE_NAME}')
-        _validate_config(parser.apikey, parser.theme_id, parser.store)
+        logging.warning(f'Could not find config file at {CONFIG_FILE}')
     else:
         with open(CONFIG_FILE, "r") as yamlfile:
-            config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+            configs = yaml.load(yamlfile, Loader=yaml.FullLoader)
             yamlfile.close()
 
-    if getattr(parser, 'apikey', None):
-        is_config_update = True
-        config['development']['apikey'] = parser.apikey
+    if configs.get(env):
+        if getattr(parser, 'apikey', None):
+            configs[env]['apikey'] = parser.apikey
 
-    if getattr(parser, 'theme_id', None):
-        is_config_update = True
-        config['development']['theme_id'] = parser.theme_id
+        if getattr(parser, 'theme_id', None):
+            configs[env]['theme_id'] = parser.theme_id
 
-    if getattr(parser, 'store', None):
-        is_config_update = True
-        config['development']['store'] = parser.store
+        if getattr(parser, 'store', None):
+            configs[env]['store'] = parser.store
+    else:
+        configs[getattr(parser, 'env')] = {
+            "apikey": getattr(parser, 'apikey', None),
+            "theme_id": getattr(parser, 'theme_id', None),
+            "store": getattr(parser, 'store', None)
+        }
+    _validate_config(configs[env]['apikey'], configs[env]['theme_id'], configs[env]['store'])
 
-    _validate_config(
-        config.get('development', {}).get('apikey'),
-        config.get('development', {}).get('theme_id'),
-        config.get('development', {}).get('store'),
+    if write_file:
+        with open(CONFIG_FILE, 'w') as yamlfile:
+            yaml.dump(configs, yamlfile)
+            yamlfile.close()
+
+    return Config(
+        env,
+        configs[env]['apikey'],
+        configs[env]['theme_id'],
+        configs[env]['store']
     )
 
-    if is_config_update:
+
+def _create_and_get_config(parser):
+    config = {}
+    env = parser.env
+    if not os.path.exists(CONFIG_FILE):
+        _validate_config(parser.apikey, parser.theme_id, parser.store)
+
+        config[env] = {
+            "apikey": getattr(parser, 'apikey', None),
+            "theme_id": getattr(parser, 'theme_id', None),
+            "store": getattr(parser, 'store', None)
+        }
         with open(CONFIG_FILE, 'w') as yamlfile:
             yaml.dump(config, yamlfile)
             yamlfile.close()
 
-    return ConfigInfo(
-        config['development']['apikey'],
-        config['development']['theme_id'],
-        config['development']['store'],
-    )
+        return Config(env, config[env]['apikey'], config[env]['theme_id'], config[env]['store'])
+    else:
+        return _get_config(parser, write_file=True)
 
 
 def _request(request_type, url, apikey=None, payload={}, files={}):
@@ -90,6 +108,38 @@ def _url(config_info):
     return f"{config_info.store}/api/admin/themes/{config_info.theme_id}/templates/"
 
 
+def progressBar(iterable, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    total = len(iterable)
+
+    # Progress Bar Printing Function
+    def printProgressBar(iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+
+    # Initial Call
+    printProgressBar(0)
+    # Update Progress Bar
+    for i, item in enumerate(iterable):
+        yield item
+        printProgressBar(i + 1)
+    # Print New Line on Complete
+    print()
+
+
 def _handle_files_change(changes, config_info):
     url = _url(config_info)
     for event_type, pathfile in changes:
@@ -100,7 +150,7 @@ def _handle_files_change(changes, config_info):
         if current_pathfile.endswith(('.py', '.yml', '.conf')):
             return
 
-        logging.info(f'{str(event_type)} {template_name}')
+        logging.info(f'[{config_info.env}] processing {template_name}')
 
         if event_type in [Change.added, Change.modified]:
             files = {}
@@ -120,6 +170,8 @@ def _handle_files_change(changes, config_info):
         elif event_type == Change.deleted:
             response = _request("DELETE", f'{url}?name={template_name}', apikey=config_info.apikey)
 
+        logging.info(f'[{config_info.env}] {str(event_type)} {template_name}')
+
         # api log error
         if not str(response.status_code).startswith('2'):
             result = response.json()
@@ -128,11 +180,11 @@ def _handle_files_change(changes, config_info):
                 error_msg = ' '.join(result.get('content', []))
             if result.get('file'):
                 error_msg = ' '.join(result.get('file', []))
-            logging.error(f'{template_name} -> {error_msg}')
+            logging.error(f'[{config_info.env}] {template_name} -> {error_msg}')
 
 
 def pull(parser):
-    config_info = _get_config(parser)
+    config_info = _create_and_get_config(parser)
 
     response = _request("GET", _url(config_info), apikey=config_info.apikey)
     templates = response.json()
@@ -142,7 +194,7 @@ def pull(parser):
         return
 
     current_files = []
-    for template in templates:
+    for template in progressBar(templates, prefix=f'[{config_info.env}] Progress:', suffix='Complete', length=50):
         template_name = str(template['name'])
         current_pathfile = os.path.join(os.getcwd(), template_name)
         current_files.append(current_pathfile.replace('\\', '/'))
@@ -163,26 +215,7 @@ def pull(parser):
                 template_file.write(template.get('content'))
                 template_file.close()
 
-        logging.info(f'Downloading {template_name}')
-
-    # find all files in directory
-    files_in_directory = []
-    for root, dirs, files in os.walk(os.getcwd()):
-        for file in files:
-            file_extension = file.split('.')[-1]
-            if f'.{file_extension}' not in CONTENT_FILE_EXTENSIONS + MEDIA_FILE_EXTENSIONS:
-                continue
-
-            files_in_directory.append(os.path.join(root, file).replace('\\', '/'))
-
-    # delete file in directory that don't exist in store
-    for file_name in files_in_directory:
-        if file_name in current_files:
-            continue
-
-        os.remove(file_name)
-        name = file_name.replace(os.getcwd(), '')
-        logging.info(f'Removing {name} -> file don\'t exist in store')
+        time.sleep(0.01)
 
 
 def watch(parser):
@@ -202,13 +235,18 @@ def create_parser():
         parser.add_argument('-s', '--store', action="store", dest="store", help='your store\'s URL')
         parser.add_argument(
             '-t', '--theme_id', action="store", dest="theme_id", help='your theme\'s ID is to download')
+        parser.add_argument(
+            '-e', '--env', action="store", dest="env", default='development',
+            help='environment to run the command (default [development])')
 
     # create the top-level parser
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(title='prime-theme-kit\'s commands', help='List command')
+    parser = argparse.ArgumentParser(
+        epilog='Use "ntk [command] --help" for more information about a command.')
+    _add_config_arguments(parser)
+    subparsers = parser.add_subparsers(title='Available Commands')
 
     # create the parser for the "pull" command
-    parser_pull = subparsers.add_parser('pull', help='To download a specific theme')
+    parser_pull = subparsers.add_parser('pull', help='Download a specific theme')
     parser_pull.set_defaults(func=pull)
     _add_config_arguments(parser_pull)
 
@@ -229,3 +267,5 @@ if __name__ == '__main__':
         logging.error(e)
     except KeyboardInterrupt:
         pass
+    except AttributeError:
+        logging.error('unknown command for "ntk", please run "ntk -h"!')
