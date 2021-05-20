006125548c8e21a3f0bbe8722ac5a059bc953d87
Alex Phelps <alexphelps3@gmail.com>
2021-05-20 14:59:00 +0700

    add readme to setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,18 @@ tests_require = [
     "nose==1.3.7"
 ]
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
 setup(
     name='next-theme-kit',
     author="29next",
     author_email="dev@29next.com",
-    version='0.1',
+    url='https://29next.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.0.1',
     install_requires=[
         "PyYAML==5.4.1",
         "requests==2.25.1",
