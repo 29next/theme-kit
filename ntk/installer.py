e49de8bd48ce4024b7333e5b8f0ba70d7b72dc0e
reloadsweety <gor.sut.lame@gmail.com>
2025-02-13 11:01:41 +0700

    update test

--- ntk/installer.py
+++ ntk/installer.py
@@ -7,9 +7,10 @@ import subprocess
 # This script is used to create a command alias (`ntk`) for running a Python module (`ntk`) more easily.
 
 # To generate an executable using PyInstaller, use the following command:
-# python -m PyInstaller --onefile --console .\ntk\installer.py -n n
+# python -m PyInstaller --onefile --console .\ntk\installer.py -n ntk
 
-if __name__ == "__main__":
+
+def auto_install_and_generate_ntk_command():
     subprocess.run(['python', '-m', 'pip', 'install',  'next-theme-kit'])
 
     # Set up a command alias: 'ntk' will execute 'python -m ntk' with any passed arguments
@@ -17,3 +18,7 @@ if __name__ == "__main__":
 
     # Open a new command prompt window and keep it open
     os.system("cmd /k")
+
+
+if __name__ == "__main__":
+    auto_install_and_generate_ntk_command()
